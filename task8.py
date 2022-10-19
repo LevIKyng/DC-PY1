427834f0e4fecd75c654e4c831a1a4cb9a74ce67
LevIKyng <115502272+LevIKyng@users.noreply.github.com>
2022-10-19 21:35:51 +0300

    Update task8.py

--- task8.py
+++ task8.py
@@ -4,11 +4,11 @@ spend = 6000
 increase = 0.05
 
 month = 0  # количество месяцев, которое можно прожить
-difference = salary - spend
 
-while money_capital + difference >= spend:
+while money_capital >= spend:
     spend += spend * increase
-    money_capital += difference
+    money_capital += salary - spend
     month += 1
 
 print(month)
+
