ea4fa9ebb33e830992ba39ce1f6c949a0f9d79c6
LevIKyng <115502272+LevIKyng@users.noreply.github.com>
2022-11-10 23:49:30 +0300

    Update task_3.py
    Третья попытка.

--- task_3.py
+++ task_3.py
@@ -1,7 +1,14 @@
-def delete(list_, index=None):
-    if index is None:
-        index = len(list_)-1
-    return list_[0:index] + list_[index+1:]
+def delete(list_, index=-1):
+    if index < 0:
+        index = len(list_)+index
+    if index >= len(list_) or index < 0:
+        return list_
+    half_1 = list_[0:index]
+    half_2 = list_[(index+1):]
+    new_list = half_1
+    for i in range(len(half_2)):
+        new_list.append(half_2[i])
+    return new_list
 
 
 print(delete([0, 0, 1, 2], index=0))  # [0, 1]
